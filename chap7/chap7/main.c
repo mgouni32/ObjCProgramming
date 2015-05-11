23e90a854a0aea0631e9517f47122c5481961c1f
mgouni32 <mgouni32@gmail.com>
2015-05-11 13:47:59 -0500

    Mounika chap 7

---
+++ chap7/chap7/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  chap7
+//
+//  Created by Mounika on 5/6/15.
+//  Copyright (c) 2015 Mounika. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    printf("3*3+5*2=%d\n",3*3+5*2);
+    printf("11/3=%d remainder of %d\n",11/3,11%3);
+    printf("11/3.0=%f\n",11/3.0);
+    printf("The Absolute Value of -5 is %d\n", abs(-5));
+        return 0;
+}
