166ddb6b9e726128773714236b5f844fa99d4fdf
mgouni32 <mgouni32@gmail.com>
2015-05-11 13:52:08 -0500

    Mounika iexifelse

---
+++ iexifelse/iexifelse/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  iexifelse
+//
+//  Created by Mounika on 5/4/15.
+//  Copyright (c) 2015 Mounika. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int a;
+if(a<100)
+{
+    printf("Enter a number less than 100 :%d ",a);
+    scanf("%d",&a);
+    printf("Good Job!!");
+}
+else
+    {
+        printf("Entered wrong Data");
+        return 0;
+    }
+}
