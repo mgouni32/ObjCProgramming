717aa40cbfe8ea56dbedac3684d5f82940d4604c
mgouni32 <mgouni32@gmail.com>
2015-05-11 15:02:28 -0500

    Mounika forloop

---
+++ forloop/forloop/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  forloop
+//
+//  Created by Mounika on 5/6/15.
+//  Copyright (c) 2015 Mounika. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    for(int i=0; i<12; i++)
+    {
+        printf(" Mounika is Cool\n",i);
+    }
+    
+    return 0;
+}
