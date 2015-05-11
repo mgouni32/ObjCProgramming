9e9a4ab412cf024c643a23972f9d08bedbb32613
mgouni32 <mgouni32@gmail.com>
2015-05-11 15:10:03 -0500

    Mounika BMICal

---
+++ BMICalc/BMICalc/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  BMICalc
+//
+//  Created by Mounika on 5/10/15.
+//  Copyright (c) 2015 Mounika. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+//Here is  the declaration of the struct
+struct Person{
+    float heightInMeters;
+    int weightInKilos;
+};
+int main(int argc, const char * argv[])
+{
+    struct Person mikey;
+    mikey.heightInMeters = 1.7;
+    mikey.weightInKilos =96;
+    
+    struct Person aaron;
+    aaron.heightInMeters = 1.97;
+    aaron.weightInKilos = 84;
+    
+    printf("mikey is %2f meters tall\n", mikey.heightInMeters);
+    printf("mikey weights %d kilograms\n", mikey.weightInKilos);
+    printf("aaron is %2f meters tall\n", aaron.heightInMeters);
+    printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
+    return 0;
+}
